edbdf1d7e231ed8f3230b0083ea71ad063173b85
Ashley Westcott <ashley.e.westcott@gmail.com>
2019-07-09 15:44:02 -0400

    tues updates

--- src/containers/TaskCreationPage.js
+++ src/containers/TaskCreationPage.js
@@ -1,45 +1,80 @@
 import React from 'react'
 import TaskCreationForm from '../components/TaskCreationForm'
 
+const API = 'http://localhost:3000/tasks'
+
 export default class TaskCreationPage extends React.Component {
 
   state = {
     title: '',
     content: '',
-    deadline: ''
+    deadline: '',
+    topic_id: []
   }
 
   handleChange = (event) => {
     this.setState({
     [event.target.name]: event.target.value
     })
+
+    console.log("handleChange before", event.target.value)
+
+    this.state.topic_id.map(topic => {
+      if (topic.id === event.target.value){
+        return this.state.topic
+      } else {
+        return topic
+      }
+
+
+      })
   }
 
-  handleSubmit = (event) => {
-    alert('Values submitted: ' + this.state.title + this.state.content + this.state.deadline);
-    event.preventDefault();
-    fetch('http://localhost:3000/tasks', {
+  componentDidMount() {
+    fetch('http://localhost:3000/topics')
+      .then(r => r.json())
+      .then(incomingTopics => {
+        console.log("fetch get for topics", incomingTopics)
+        this.setState({
+          topic_id: incomingTopics
+          })
+        })
+  }
+
+  postTasks = () => {
+    fetch(API, {
       method: 'POST',
-      mode: 'no-cors',
       headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
       },
       body: JSON.stringify({
-        "title": this.state.title.value,
-        "content": this.state.content.value,
-        "deadline": this.state.deadline.value
+        title: this.state.title,
+        content: this.state.content,
+        topic_id: this.state.topic_id,
+        deadline: this.state.deadline
         })
       })
-      .then(response => response.json())
-      .then(newdata => console.log(newdata))
+      // .then(response => response.json())
+      // .then(newdata => console.log(newdata))
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.postTasks()
+  }
+
+  foundTopic(){
+    this.props.topic_id.find((element) => {return element.id})
   }
 
   render(){
     console.log(this.state)
     return (
     <div>
-      <TaskCreationForm handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
+      <TaskCreationForm
+      foundTopic={this.foundTopic}
+      topic_id={this.state.topic_id} handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
     </div>
     )
   }
